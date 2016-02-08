bde8875280c9190d13b9c89faa77836876b701bd
harej <jamesmhare@gmail.com>
2016-02-08 03:26:31 -0500

    Undo

--- views.py
+++ views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-from django.template import Context
 
 def select_language(request):
     available = {
                      'en': 'English'
                 }
 
-    context = Context({
+    context = {
                   'language': 'en',
                   'languages_available': available
-              })
+              }
 
     return render(request, 'requestoid/select_language.html', context = context)
