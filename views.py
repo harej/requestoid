e8420903f791aa5cba6e9d21f767e53b235d14f0
harej <jamesmhare@gmail.com>
2016-02-08 03:39:50 -0500

    What about list of lists?

--- views.py
+++ views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def select_language(request):
     available = [
-                     ('en', 'English')
+                     ['en', 'English']
                 ]
 
     context = {
