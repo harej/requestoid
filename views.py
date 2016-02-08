9fc00b0f0a501dc257b9f5c9a4f24e9caaec0731
harej <jamesmhare@gmail.com>
2016-02-08 01:59:17 -0500

    Removing hard coded things

--- views.py
+++ views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 
 def select_language(request):
-    return render(request, 'requestoid/select_language.html', context={'language': 'en'})
+	languages_available = {
+	                         'en': 'English'
+	                      }
+
+    return render(request, 'requestoid/select_language.html', context={'language': 'en', 'languages_available': languages_available})
