a132d849c23e530d92e738ae4d7ac13754386960
harej <jamesmhare@gmail.com>
2016-02-08 00:45:21 -0500

    Language selector: setting English as language for lack of user-set language

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import get_object_or_404, render
 
 def select_language(request):
-    return render(request, 'requestoid/select_language.html', {})
+    return render(request, 'requestoid/select_language.html', {'language': 'en'})
