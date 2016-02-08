03202b0f675b9e4cd1e8dd651c78eb546c348431
harej <jamesmhare@gmail.com>
2016-02-08 16:08:32 -0500

    Replacing hard-coded interface messages with translatable strings via gettext

--- views.py
+++ views.py
@@ -1,14 +1,36 @@
+import gettext
 from django.shortcuts import render
 
-def select_language(request):
-    available = [
-                     {'code': 'en', 'label': 'English'},
-                     {'code': 'fr', 'label': 'français'}
-                ]
+def interface_messages(langcode):
+    '''
+    Provides a dictionary to feed into the context, with an ISO 639-1 or -2 language code as input.
+    '''
 
-    context = {
-                  'language': 'en',
-                  'available': available
-              }
+    translation = gettext.translation('interface', localedir='locale', languages=[langcode])
+    translation.install()
 
-    return render(request, 'requestoid/select_language.html', context = context)
+    output = {
+                'brand': _('Wikipedia Requests'),
+                'login_via_wikipedia': _('Login via Wikipedia'),
+                'search': _('Search'),
+                'add': _('Add'),
+                'help': _('Help'),
+                'about': _('About')
+             }
+
+    return output
+
+
+class Views:
+    def select_language(self, request):
+        available = [
+                         {'code': 'en', 'label': 'English'}
+                    ]
+
+        context = {
+                      'interface': interface_messages('en'),
+                      'language': 'en',
+                      'available': available
+                  }
+
+        return render(request, 'requestoid/select_language.html', context = context)
