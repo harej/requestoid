847cdb88892117df99fc1a678b77697aaff7f78d
harej <jamesmhare@gmail.com>
2016-02-08 03:23:58 -0500

    Invoking Context class directly

--- views.py
+++ views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
+from django.template import Context
 
 def select_language(request):
     available = {
                      'en': 'English'
                 }
 
-    context = {
+    context = Context({
                   'language': 'en',
                   'languages_available': available
-              }
+              })
 
     return render(request, 'requestoid/select_language.html', context = context)
