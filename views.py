91ed0ac524f660ab5e4d1f7751fbdc80e336f161
harej <jamesmhare@gmail.com>
2016-02-08 02:41:34 -0500

    Still working on this weird issue

--- views.py
+++ views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
 def select_language(request):
-	languages_available = {
-	                          'en': 'English'
-	                      }
+	available = {
+	                 'en': 'English'
+	            }
 
 	context = {
 	              'language': 'en',
-	              'languages_available': languages_available
+	              'languages_available': available
 	          }
 
     return render(request, 'requestoid/select_language.html', context = context)
