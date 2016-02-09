a282cd09061e1e48f00be736459f74ec06bec6fe
harej <jamesmhare@gmail.com>
2016-02-09 13:34:01 -0500

    ugh

--- views.py
+++ views.py
@@ -67,8 +67,8 @@ def homepage(request, langcode):  # /requests/en
 def auth(request, langcode):  # /requests/en/auth
     keyfile = configparser.ConfigParser()
     keyfile.read([os.path.expanduser('~/.oauth.ini')])
-    consumer_key = loginfile.get('oauth', 'consumer_key')
-    consumer_secret = loginfile.get('oauth', 'consumer_secret')
+    consumer_key = keyfile.get('oauth', 'consumer_key')
+    consumer_secret = keyfile.get('oauth', 'consumer_secret')
     consumer_token = ConsumerToken(consumer_key, consumer_secret)
     handshaker = Handshaker("https://en.wikipedia.org/w/index.php", consumer_token)
     redirect, request_token = handshaker.initiate()
