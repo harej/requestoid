0957579e973206aa6a34486a31ab7a65b65af97e
harej <jamesmhare@gmail.com>
2016-03-01 18:13:51 -0500

    Proper registration

--- templatetags/wikipedia_link.py
+++ templatetags/wikipedia_link.py
@@ -2,5 +2,6 @@ from django import template
 
 register = template.Library()
 
+@register.filter
 def wikipedia_link(pagetitle, langcode):
     return '<a href="https://{0}.wikipedia.org/wiki/{1}">{2}</a>'.format(langcode, pagetitle.replace(' ', '_'), pagetitle)
