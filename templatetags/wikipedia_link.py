011a7f95f9bab349e3d26b167a124aa419530557
harej <jamesmhare@gmail.com>
2016-03-01 18:11:58 -0500

    Changes to request view appearance

---
+++ templatetags/wikipedia_link.py
@@ -0,0 +1,6 @@
+from django import template
+
+register = template.Library()
+
+def wikipedia_link(pagetitle, langcode):
+    return '<a href="https://{0}.wikipedia.org/wiki/{1}">{2}</a>'.format(langcode, pagetitle.replace(' ', '_'), pagetitle)
