6c4afdf30c82bbea59121bb8b75dbc8948b91767
harej <jamesmhare@gmail.com>
2016-03-01 18:15:19 -0500

    Trying that again

--- templatetags/wikipedia_link.py
+++ templatetags/wikipedia_link.py
@@ -2,6 +2,7 @@ from django import template
 
 register = template.Library()
 
-@register.filter
 def wikipedia_link(pagetitle, langcode):
     return '<a href="https://{0}.wikipedia.org/wiki/{1}">{2}</a>'.format(langcode, pagetitle.replace(' ', '_'), pagetitle)
+
+register.filter('wikipedia_link', wikipedia_link)
