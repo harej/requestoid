9f32a0adb58911194b93b46160cca19236ba0e09
harej <jamesmhare@gmail.com>
2016-03-01 18:42:59 -0500

    using format_html

--- templatetags/wikipedia_link.py
+++ templatetags/wikipedia_link.py
@@ -1,7 +1,8 @@
 from django import template
+from django.utils.html import format_html
 
 register = template.Library()
 
 @register.filter
 def wikipedia_link(pagetitle, langcode):
-    return '<a href="https://{0}.wikipedia.org/wiki/{1}">{2}</a>'.format(langcode, pagetitle.replace(' ', '_'), pagetitle)
+    return format_html('<a href="https://{}.wikipedia.org/wiki/{}">{}</a>', langcode, pagetitle.replace(' ', '_'), pagetitle)
