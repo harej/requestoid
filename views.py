71757f3a622af9ab828bb84a77aa550392fa46ce
harej <jamesmhare@gmail.com>
2016-04-02 16:17:49 +0300

    Abstracting request retrieving logic so that it can be re-used

--- views.py
+++ views.py
@@ -72,6 +72,30 @@ def interface_messages(request, langcode):
 
     return output
 
+def retrieve_requests(g):
+    searchterm = g['searchterm']
+    database = g['language'] + 'wiki'
+    searchterm = searchterm.replace('_', ' ')
+    searchtype = g['searchtype']
+
+    if searchtype == 'article':
+        searchterm = wiki.RedirectResolver(g['language'], searchterm, 0)
+        R = models.Requests.objects.filter(page_title=searchterm, wiki=database, status=0)
+    elif searchtype == 'category':
+        if searchterm[:9] == 'Category:':
+            searchterm = searchterm[9:]
+        C = models.Categories.objects.filter(cat_title=searchterm, wiki=database, request__status=0)
+        R = [entry.request for entry in C]
+    elif searchtype == 'wikiproject':
+        if searchterm[:10] == 'Wikipedia:':
+            searchterm = searchterm[10:]
+        elif searchterm[:3] == "WP:":
+            searchterm = searchterm[3:]
+        searchterm = wiki.RedirectResolver(g['language'], searchterm, 4)
+        W = models.WikiProjects.objects.filter(project_title=searchterm, wiki=database, request__status=0)
+        R = [entry.request for entry in W]
+
+    return R
 
 ###
 
@@ -517,27 +541,7 @@ def search(request, langcode):  # /requests/en/search
     g = request.GET
     if 'searchterm' in g:
         if g['searchterm'] != '' and g['searchterm'] != ' ':
-            searchterm = g['searchterm']
-            database = g['language'] + 'wiki'
-            searchterm = searchterm.replace('_', ' ')
-            searchtype = g['searchtype']
-
-            if searchtype == 'article':
-                searchterm = wiki.RedirectResolver(g['language'], searchterm, 0)
-                R = models.Requests.objects.filter(page_title=searchterm, wiki=database, status=0)
-            elif searchtype == 'category':
-                if searchterm[:9] == 'Category:':
-                    searchterm = searchterm[9:]
-                C = models.Categories.objects.filter(cat_title=searchterm, wiki=database, request__status=0)
-                R = [entry.request for entry in C]
-            elif searchtype == 'wikiproject':
-                if searchterm[:10] == 'Wikipedia:':
-                    searchterm = searchterm[10:]
-                elif searchterm[:3] == "WP:":
-                    searchterm = searchterm[3:]
-                searchterm = wiki.RedirectResolver(g['language'], searchterm, 4)
-                W = models.WikiProjects.objects.filter(project_title=searchterm, wiki=database, request__status=0)
-                R = [entry.request for entry in W]
+          R = retrieve_requests(g)
 
             content = {'search_term': searchterm,
                        'search_type': _(searchtype[0].upper() + searchtype[1:]),
