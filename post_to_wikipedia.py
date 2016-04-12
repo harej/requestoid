c6042cfefc87abf63538ce59e8abd505375aeedb
harej <jamesmhare@gmail.com>
2016-04-12 14:54:32 -0400

    Working

--- post_to_wikipedia.py
+++ post_to_wikipedia.py
@@ -1,6 +1,6 @@
 import os
 import django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "requestoid.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 django.setup()
 
 import pywikibot
