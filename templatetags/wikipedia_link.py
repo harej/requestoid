010fb2c5a3573a8d0a24551c1a8a86a6bf24d639
harej <jamesmhare@gmail.com>
2016-04-19 18:44:45 -0400

    Moving red link logic to template tag

--- templatetags/wikipedia_link.py
+++ templatetags/wikipedia_link.py
@@ -4,5 +4,8 @@ from django.utils.html import format_html
 register = template.Library()
 
 @register.filter
-def wikipedia_link(pagetitle, langcode):
-    return format_html('<a href="https://{}.wikipedia.org/wiki/{}">{}</a>', langcode, pagetitle.replace(' ', '_'), pagetitle)
+def wikipedia_link(pagetitle, langcode, page_id):
+    if page_id == 0:
+        return format_html('<a class="new" href="https://{}.wikipedia.org/wiki/{}">{}</a>', langcode, pagetitle.replace(' ', '_'), pagetitle)
+    else:
+        return format_html('<a href="https://{}.wikipedia.org/wiki/{}">{}</a>', langcode, pagetitle.replace(' ', '_'), pagetitle)
