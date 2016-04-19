431f34dc5fd35623366c541ab654ce43ad9e6aa0
harej <jamesmhare@gmail.com>
2016-04-19 18:51:39 -0400

    Separate template tag for red links

--- templatetags/wikipedia_link.py
+++ templatetags/wikipedia_link.py
@@ -4,8 +4,9 @@ from django.utils.html import format_html
 register = template.Library()
 
 @register.filter
-def wikipedia_link(pagetitle, langcode, page_id):
-    if page_id == 0:
-        return format_html('<a class="new" href="https://{}.wikipedia.org/wiki/{}">{}</a>', langcode, pagetitle.replace(' ', '_'), pagetitle)
-    else:
-        return format_html('<a href="https://{}.wikipedia.org/wiki/{}">{}</a>', langcode, pagetitle.replace(' ', '_'), pagetitle)
+def wikipedia_link(pagetitle, langcode):
+    return format_html('<a href="https://{}.wikipedia.org/wiki/{}">{}</a>', langcode, pagetitle.replace(' ', '_'), pagetitle)
+
+@register.filter
+def wikipedia_red_link(pagetitle, langcode):
+    return format_html('<a class="new" href="https://{}.wikipedia.org/wiki/{}">{}</a>', langcode, pagetitle.replace(' ', '_'), pagetitle)
